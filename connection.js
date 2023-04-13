0c7942aad5d0eab8794317180026f8c74fceff85
konchellah <williamoriginal2019@gmail.com>
2023-04-13 03:30:14 +0300

    error db conn

--- connection.js
+++ connection.js
@@ -14,13 +14,20 @@ const DB_NAME = process.env.DB_NAME;
 const DB_USER = process.env.DB_USER;
 const DB_PASS = process.env.DB_PASS;
 const DB_INSTANCE = process.env.INSTANCE_CONNECTION_NAME;
+const DB_PUB_IP = process.env.DB_PUB_IP;
+
+console.log(`DB_NAME = ${DB_NAME} \n
+    DB_USER = ${DB_USER} \n
+    DB_PASS = ${DB_PASS} \n
+    DB_INSTANCE = ${INSTANCE_CONNECTION_NAME} \n
+    DB_PUB_IP = ${DB_PUB_IP}`)
 
 const sequelize = new Sequelize(
     DB_NAME,
     DB_USER,
     DB_PASS, {
 
-    host: DB_INSTANCE,
+    host: DB_PUB_IP,
     dialect: 'postgres',
     dialectOptions: {
         socketPath: `/cloudsql/${DB_INSTANCE}`
