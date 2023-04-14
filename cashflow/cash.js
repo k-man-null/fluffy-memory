a2acb2c71389c44f04b40fcdcdcbae5a4ff9768c
konchellah <williamoriginal2019@gmail.com>
2023-04-14 18:47:09 +0300

    Image upload does not therow error if there is no gcs bucket, however works well if there is bucket, also added payments listing wallet

--- cashflow/cash.js
+++ cashflow/cash.js
@@ -3,55 +3,72 @@ const IntaSend = require('intasend-node');
 const intasendPublishable = process.env.INTASEND_PUBLISHABLE_TOKEN;
 const intasendSecret = process.env.INTASEND_SECRET_TOKEN;
 
-
-
 function listWallets() {
 
-    let intasend = new IntaSend(
-        publishable_key = intasendPublishable,
-        secret_key = intasendSecret,
-        test_mode=false
-    );
+    let intasend;
+
+    if (intasendPublishable && intasendSecret) {
+
+        console.log(`Intasend publishable ${intasendPublishable} \n
+        Intasendsecret ${intasendSecret}`);
+
+        intasend = new IntaSend(
+            intasendPublishable,
+            intasendSecret,
+            false
+        );
+
+        console.log("Listing wallets...........\n\n")
+        let wallets = intasend.wallets();
+        wallets
+            .list()
+            .then((resp) => {
+                console.log(`Response: ${JSON.stringify(resp)}`);
+            })
+            .catch((err) => {
+                console.error(`Error: ${err}`);
+            });
+
+        console.log("\n\n\nListing wallets...........")
+
+    }
 
 
-    console.log("Listing wallets...........\n\n")
-    let wallets = intasend.wallets();
-    wallets
-        .list()
-        .then((resp) => {
-            console.log(`Response: ${JSON.stringify(resp)}`);
-        })
-        .catch((err) => {
-            console.error(`Error: ${err}`);
-        });
-
-    console.log("\n\n\nListing wallets...........")
 }
 
 function createWallet() {
 
-    let intasend = new IntaSend(
-        publishable_key = intasendPublishable,
-        secret_key = intasendSecret,
-        test_mode=false
-    );
+    let intasend;
 
-    
+    if (intasendPublishable && intasendSecret) {
 
-    let wallets = intasend.wallets();
-    wallets
-        .create({
-            label: 'NodeJS-SDK-TEST',
-            wallet_type: 'WORKING',
-            currency: 'KES',
-            can_disburse: false
-        })
-        .then((resp) => {
-            console.log(`Response: ${JSON.stringify(resp)}`);
-        })
-        .catch((err) => {
-            console.error(`Error: ${err}`);
-        });
+        console.log(`Intasend publishable ${intasendPublishable} \n
+        Intasendsecret ${intasendSecret}`);
+
+        intasend = new IntaSend(
+            intasendPublishable,
+            intasendSecret,
+            false
+        );
+
+        let wallets = intasend.wallets();
+        wallets
+            .create({
+                label: 'NodeJS-SDK-TEST',
+                wallet_type: 'WORKING',
+                currency: 'KES',
+                can_disburse: false
+            })
+            .then((resp) => {
+                console.log(`Response: ${JSON.stringify(resp)}`);
+            })
+            .catch((err) => {
+                console.error(`Error: ${err}`);
+            });
+    }
+
+
+
 }
 
 createWallet();
