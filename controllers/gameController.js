adad28ae6104e3762370a7c21a4949e75f219bfa
konchellah <williamoriginal2019@gmail.com>
2023-05-01 21:57:28 +0300

    Migrating to Firestore => getGame

--- controllers/gameController.js
+++ controllers/gameController.js
@@ -217,10 +217,10 @@ async function getMyLiveGames(req, res) {
         console.log(id);
 
         const gamesRef = db.collection('games');
-        const query = gamesRef.where('host_id', '==', id).where('status', '==', 'live');
+        const query = gamesRef.where('host_id', '==', `${id}`).where('status', '==', 'live');
         const querySnapshot = await query.get();
 
-        console.log(querySnapshot);
+        
 
         if (!querySnapshot.exists) {
             return res.status(400).json({ message: "You have no live games" });
