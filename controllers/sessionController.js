49e9a06d7a66b3c81c04e067b937ba9f7b0db50f
konchellah <williamoriginal2019@gmail.com>
2023-06-12 16:02:54 +0300

    Error verifying email

--- controllers/sessionController.js
+++ controllers/sessionController.js
@@ -72,7 +72,7 @@ async function verifyEmail(req, res) {
         const to = req.user.email;
 
         const code = jwt.sign(req.user, "myprivatekeytochange", {
-            expiresIn: 300
+            expiresIn: 3000
         });
 
         const text = "To verify your email, click the link below. The link is only valid for 5 minutes"
