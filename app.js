1febfada998aa3349e92b12428098d7e27f2edcd
konchellah <williamoriginal2019@gmail.com>
2023-07-22 21:38:19 +0300

    Fix

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const cookieparser = require('cookie-parser');
 const cron = require('node-cron');
 const { endGame } = require('./utils/giveprizes');
 
-cron.schedule('* 1 * * * *', () => {
+cron.schedule('* * * * *', () => {
     endGame();
 });
 
